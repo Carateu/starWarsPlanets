4c7de3901abb6cd7594c683809d5221dbb78999e
Bruno Carateu <karateu@outlook.com>
2020-03-13 12:44:33 -0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+import './Button.css'
+import React from 'react'
+
+export default props =>
+<button onClick={()=> window.location.reload(false)}
+className="button"
+>
+NEXT
+</button>
+
---
+++ src/main/StarApp.jsx
@@ -0,0 +1,62 @@
+import './StarApp.css'
+import React,{Component} from 'react'
+import Button from '../components/Button'
+
+export default class starApp extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            planet: {
+               name:'',
+               population:'',
+               climate:'',
+               terrain:'',
+               films: [],
+            }
+        }
+    }
+
+    componentDidMount(){
+        const id = Math.floor((Math.random()*20)+1)
+        fetch(`https://swapi.co/api/planets/${id}/`)
+        .then(results => results.json())
+        .then(results => this.setState(
+            {planet:{
+                name : results.name,
+                population : results.population,
+                climate : results.climate,
+                terrain : results.terrain,
+                films : results.films,
+                }
+            }
+            ))
+            
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                    <div className="div3">
+                        {(this.state.planet.name)}
+                    </div>
+                    
+                    <div className='div4'>
+                        <ul>
+                            <li><bold>Population: </bold>
+                            {(this.state.planet.population)}</li>
+                            <li><bold>Climate: </bold>
+                            {(this.state.planet.climate)}</li>
+                            <li><bold>Terrain: </bold>
+                            {(this.state.planet.terrain)}</li>
+                       </ul>
+                       <p>Featured in: {(this.state.planet.films.length)} films</p>
+                    </div>    
+                    
+                <Button></Button>
+            </div>
+        )
+    }
+}
